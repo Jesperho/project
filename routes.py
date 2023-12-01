ffbc7187718cd26c7ad93a115b34406fb6feadf8
Anatolii Kuzhlev <anatolii.kuzhlev@tuni.fi>
2023-12-01 13:59:30 +0200

    added some routes

--- routes.py
+++ routes.py
@@ -3,6 +3,23 @@ import game
 
 app = Flask(__name__)
 
-@app.route("/id")
+@app.get("/id")
 def new_id():
     return {"id": game.new_id()}
+
+@app.post("/players/<name>")
+def create_player(name: str):
+    player_id = game.create_player(name)
+    return {
+        "id": player_id,
+        "name": name
+    }
+
+@app.post("/airports/<icao>/coordinates")
+def get_airport_coordinates(icao: str):
+    coordinates_tuple = game.get_airport_coordinates(icao)
+    return {
+        "latitude": coordinates_tuple[0],
+        "longitude": coordinates_tuple[1] 
+    }
+
