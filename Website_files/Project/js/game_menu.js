e2c928bf4416b6a0a0181d92538047439d0b4519
Jesperho <142903016+Jesperho@users.noreply.github.com>
2023-12-11 22:03:01 +0200

    Add files via upload

---
+++ Website_files/Project/js/game_menu.js
@@ -0,0 +1,46 @@
+const map = L.map('map');
+
+ // Get the tile layer from OpenStreetMaps 
+ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { 
+ // Specify the maximum zoom of the map 
+ maxZoom: 19, 
+ // Set the attribution for OpenStreetMaps 
+ attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' 
+ }).addTo(map); 
+
+ // Set the view of the map 
+ // with the latitude, longitude and the zoom value 
+ map.setView([48.8584, 2.2945], 2);
+
+ function showMenu(menu) {
+ // Hide all menus
+ document.getElementById('playMenu').style.display = 'none';
+ document.getElementById('tutorialMenu').style.display = 'none';
+ document.getElementById('aboutMenu').style.display = 'none';
+
+ // Show the selected menu
+ document.getElementById(menu + 'Menu').style.display = 'block';
+
+ // Show the popup screen
+ document.getElementById('popup').style.display = 'block';
+ }
+
+ function showAboutPopup() {
+ // Show the about popup screen
+ document.getElementById('aboutPopup').style.display = 'block';
+ }
+
+ function closeAboutPopup() {
+ // Hide the about popup screen
+ document.getElementById('aboutPopup').style.display = 'none';
+ }
+
+ function showTutorialPopup() {
+    // Show the about popup screen
+    document.getElementById('tutorialPopup').style.display = 'block';
+    }
+   
+    function closeTutorialPopup() {
+    // Hide the about popup screen
+    document.getElementById('tutorialPopup').style.display = 'none';
+    }
